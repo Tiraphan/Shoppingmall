859677dcccc490f5c1f360c412d9270d2543559c
toom <tiraphan@gmail.com>
2024-11-19 18:12:46 +0700

    Start App

---
+++ lib/states/authen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Authen extends StatefulWidget {
+  const Authen({super.key});
+
+  @override
+  State<Authen> createState() => _AuthenState();
+}
+
+class _AuthenState extends State<Authen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Text('This is Authen'),
+      ),
+    );
+  }
+}
---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  //General
+  static String appName = 'Shopping Mall';
+
+  //Route
+  static String routeAuthen = '/authen';
+  static String routeCreateAccount = '/createAccount';
+  static String routeBuyerService = 'buyerService';
+  static String routeSalesService = 'salesService';
+  static String routeRiderService = 'riderService';
+}
