687f7ced4a431f8915f6a7d67a5c7c34e20e14c8
toom <tiraphan@gmail.com>
2024-11-20 17:41:52 +0700

    Start App

--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shoppingmall/states/widget/show_image.dart';
+import 'package:shoppingmall/states/widget/show_title.dart';
 import 'package:shoppingmall/utility/my_constant.dart';
 
 class Authen extends StatefulWidget {
@@ -15,11 +16,34 @@ class _AuthenState extends State<Authen> {
     double size = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
-        child: Container(
+        child: ListView(
+          children: [
+            buildImage(size),
+            buildAppName(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row buildAppName() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ShowTitle(title: MyConstant.appName, textStyle: MyConstant().h1Style()),
+      ],
+    );
+  }
+
+  Row buildImage(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
           width: size * 0.6,
           child: ShowImage(pathImage: MyConstant.image1),
         ),
-      ),
+      ],
     );
   }
 }
--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class MyConstant {
   //General
   static String appName = 'Shopping Mall';
@@ -14,4 +16,26 @@ class MyConstant {
   static String image2 = 'images/image2.png';
   static String image3 = 'images/image3.png';
   static String image4 = 'images/image4.png';
+
+  //Color
+  static Color primary = Color(0xff87861d);
+  static Color dark = Color(0xff575900);
+  static Color light = Color(0xffb9b64e);
+
+  //Style
+  TextStyle h1Style() => TextStyle(
+        fontSize: 24,
+        color: dark,
+        fontWeight: FontWeight.bold,
+      );
+  TextStyle h2Style() => TextStyle(
+        fontSize: 18,
+        color: dark,
+        fontWeight: FontWeight.w700,
+      );
+  TextStyle h3Style() => TextStyle(
+        fontSize: 14,
+        color: dark,
+        fontWeight: FontWeight.normal,
+      );
 }
